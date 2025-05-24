0c18c670d7275bf0f0a5353b6a7042f1de94209d
TejasSomnathe <tejassomnathe@gmail.com>
2025-05-24 22:11:25 +0530

    arrange the files

--- XooMart/frontend/src/main.jsx
+++ XooMart/frontend/src/main.jsx
@@ -1,10 +1,40 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./Layout.jsx";
+import Home from "./components/Home.jsx";
+import Product from "../src/components/Product/Product.jsx";
+import Stores from "../src/components/Stores/Stores.jsx";
+import Login from "../src/components/Login/Login.jsx";
+import Cart from "../src/components/Cart/Cart.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      { path: "product", element: <Product /> },
+      { path: "store", element: <Stores /> },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "cart",
+        element:<Cart />
+      }
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
