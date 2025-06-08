0662f56854c93907aef20376187c9bc6cd9658e9
TejasSomnathe <tejassomnathe@gmail.com>
2025-06-08 17:05:10 +0530

    creating add products controller

--- XooMart/backend/src/controllers/product.controller.js
+++ XooMart/backend/src/controllers/product.controller.js
@@ -1,7 +1,41 @@
 import { Product } from "../models/product.model";
 import { asyncHandler } from "../utils/asyncHandler";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
- 
+const addProduct = asyncHandler(async (req, res) => {
+  const { name, description, price, category, stockQuantity } = req.body;
+  if (!name || !description || !price || !category) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  const imageUrl = req.file ? req.file.path : null;
+
+  const cloudinaryUrl = await uploadOnCloudinary(imageUrl);
+  if (!cloudinaryUrl) {
+    return res.status(500).json({ message: "Failed to upload product image" });
+  }
+
+  const product = await Product.create({
+    name,
+    description,
+    price,
+    category,
+    stockQuantity: stockQuantity || 0,
+    imageUrl: cloudinaryUrl,
+  });
+
+  if (!product) {
+    throw new ApiError(
+      500,
+      "Something went wrong while registering the product"
+    );
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, product, "User register succesfully"));
+});
+
 const getProductById = asyncHandler(async (req, res) => {
   const productId = req.params.id;
   const product = await Product.findById(productId);
@@ -27,8 +61,8 @@ const viewProduct = asyncHandler(async (req, res) => {
     price: product.price,
     category: product.category,
     stockQuantity: product.stockQuantity,
-    imageUrl: product.imageUrl
-  })
-})
+    imageUrl: product.imageUrl,
+  });
+});
 
-export { getProductById, viewProduct }
+export { getProductById, viewProduct, addProduct };
