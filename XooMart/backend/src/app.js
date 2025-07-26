f1a64052bb72a9442aa0b56d600d01d02d1a6ce6
TejasSomnathe <tejassomnathe@gmail.com>
2025-07-26 13:25:02 +0530

    fix the cors eroor at backend

--- XooMart/backend/src/app.js
+++ XooMart/backend/src/app.js
@@ -4,12 +4,14 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173",
-//     credentials: true,
-//   })
-// );
+const allowedOrigins = ["https://xoo-mart.vercel.app"];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
